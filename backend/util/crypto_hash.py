d9574bc15bed5b49ff3713414fbfe4a4603d66a5
NoamOssia <nomason1991@gmail.com>
2020-05-06 15:10:01 +0300

    Added tests to check if the valid block works correctly

--- backend/util/crypto_hash.py
+++ backend/util/crypto_hash.py
@@ -6,12 +6,7 @@ def crypto_hash(*args):
     return a sha-256 hash of the given arguments
     """
     stringified_args= sorted(map(lambda data: json.dumps(data), args))
-
     joined_data = ''.join(stringified_args)
-
-
-
-
     return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
 
 def main():
